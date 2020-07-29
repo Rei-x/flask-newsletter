51673f3ddf062badfdf9b476103253da5aa6bb42
Rei-666 <wszyzdgo@gmail.com>
2020-07-29 14:56:54 +0200

    Create newsletter class that holds all the interactions with database and smtp server, create error codes that holds error displaying

---
+++ newsletter/error_codes.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+
+error_codes = {
+    "BadRequest": "Bad request",
+    "InvalidEmail": "Invalid email",
+    "AlreadySignedUp": "Email is already signed up",
+    "Operational": "Operational error",
+    "Unexpected": "Unexpected error",
+    "DoesntExist": "Email doesn't exist"
+}
+
+
+def error(error_code):
+    return jsonify(success=False, error=error_codes.get(error_code, "Invalid error code"), error_code=error_code)
