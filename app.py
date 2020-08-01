fa744eaac1ccc592206f3083dedfca975e69683e
Rei-666 <wszyzdgo@gmail.com>
2020-08-01 13:51:41 +0200

    Create source code for newsletter class, add gitignore file

--- app.py
+++ app.py
@@ -1,150 +1,50 @@
-from flask import Flask, request, redirect, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug import exceptions
-from flask_mail import Mail
-from flask_hashing import Hashing
-from smtplib import SMTPRecipientsRefused
-from sqlalchemy.exc import IntegrityError, OperationalError
+from flask import Flask
+from newsletter.flask_newsletter import Newsletter
+import config as cfg
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-hashing = Hashing(app)
+app.config['NEWSLETTER_TEMPLATE'] = 'index.html'
+app.config['NEWSLETTER_EMAIL_TITLE'] = "Potwierdź rejestracje"
 
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT='465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = 'wszyzdgo@gmail.com',
-    MAIL_PASSWORD='ugtgznuprzejxznl'
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=cfg.MAIL_ADDRESS,
+    MAIL_PASSWORD=cfg.MAIL_PASSWORD
 )
-mail = Mail(app)
 
+app.config['SECRET_KEY'] = cfg.SECRET
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    
-    def __repr__(self):
-        return "<Task %r>" % self.id
-
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    surname = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(20), nullable=False, unique=True)
-    confirmed = db.Column(db.Boolean, nullable=False, default=False)
-    hashed_email = db.Column(db.String(128))
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.__set_hashed_email()
-
-    def __repr__(self):
-        return f"User('{self.name}', '{self.surname}', '{self.email}', '{self.confirmed}')"
-
-    def __set_hashed_email(self):
-        self.hashed_email = hashing.hash_value(self.email, "pieseczek")
+newsletter = Newsletter(app)
 
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Lipa się stała'
-    else:
-        print()
-        clients = Client.query.all()
-        tmp = ""
-        for client in clients:
-            tmp += str(client) + "\n"
-        return tmp
+    clients = newsletter.Client.query.filter_by(confirmed=True).all()
+    tmp = ""
+    for client in clients:
+        tmp += str(client) + "<br>"
+    return tmp
 
 
 @app.route("/add_email", methods=["POST"])
-def new_email_to_newsletter():
-
-    try:
-        email = request.form['email']
-        name = request.form['name']
-        surname = request.form['surname']
-    except exceptions.BadRequestKeyError as e:
-        return e
-
-    new_user = Client(name=name, surname=surname, email=email)
-
-    if client_exists(new_user):
-        return jsonify("{'Succes': False,"
-                       "'Error': 'Email is already signed up'}")
-    elif email_exists(new_user):
-        return send_email(new_user)
-
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-    except IntegrityError:
-        return jsonify("{'Succes': False,"
-                       "'Error': Email}")
-    except OperationalError:
-        return jsonify("{'Succes': False,"
-                       "'Error' : 'OperationalError' }")
-
-    return send_email(new_user)
+def add_email():
+    return newsletter.new_email_to_newsletter()
 
 
-@app.route("/confirm_email?id=<email_hash>")
-def confirm_email(email_hash):
-    user = Client.query.filter_by(hashed_email=email_hash, confirmed=False).first()
-    if user is not None:
-        user.confirmed = True
-        db.session.commit()
-        return f"Jej udało Ci się zarejestrować do naszego newslettera {user.name}!!!!!"
-    else:
-        return jsonify("'Succes': False,"
-                       "'Error': 'Email doesn't exist'")
+@app.route("/remove_email")
+def remove_email():
+    return newsletter.remove_email()
 
 
-@app.route("/remove_email?id=<hashed>")
-def remove_email(hashed):
-    user = Client.query.filter_by(hashed_email=hashed, confirmed=True).first()
-    if user is not None:
-        user.confirmed = False
-        db.session.commit()
-        return f"Wypisaliśmy Cię z naszego newslettera kurwo"
-    else:
-        return "co xd"
+@app.route("/confirm_email")
+def confirm_email():
+    return newsletter.confirm_email()
 
-
-def send_email(user):
-    try:
-        mail.send_message('Potwierdź rejestracje',
-                          recipients=[str(user.email)],
-                          html=render_template("index.html", url=request.url_root, email_hash=user.hashed_email),
-                          sender="wszyzdgo@gmail.com")
-    except SMTPRecipientsRefused:
-        return jsonify("{'Succes': False,"
-                       "'Error': 'Bad email address'")
-    return jsonify("{'Succes': True}")
-
-
-def client_exists(client):
-    if Client.query.filter_by(email=client.email, confirmed=True).first() is not None:
-        return True
-    else:
-        return False
-def email_exists(email):
-    if Client.query.filter_by(email=email.email).first() is not None:
-        return True
-    else:
-        return False
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='192.168.0.100', debug=True)
---
+++ newsletter/flask_newsletter.py
@@ -0,0 +1,131 @@
+from flask import request, render_template, jsonify, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug import exceptions
+from flask_mail import Mail
+from flask_hashing import Hashing
+from smtplib import SMTPRecipientsRefused
+from sqlalchemy.exc import IntegrityError, OperationalError
+from newsletter.error_codes import error
+import re
+
+
+class Newsletter:
+
+    def __init__(self, app):
+
+        self.db = db = SQLAlchemy(app)
+        self.hashing = hashing = Hashing(app)
+        self.mail = Mail(app)
+
+        self.mail_username = app.config['MAIL_USERNAME']
+        self.template_file = app.config['NEWSLETTER_TEMPLATE']
+        self.email_title = app.config['NEWSLETTER_EMAIL_TITLE']
+        _SALT = app.config['SECRET_KEY']
+
+        class Client(db.Model):
+            id = db.Column(db.Integer, primary_key=True)
+            name = db.Column(db.String(20), nullable=False)
+            surname = db.Column(db.String(20), nullable=False)
+            email = db.Column(db.String(20), nullable=False, unique=True)
+            confirmed = db.Column(db.Boolean, nullable=False, default=False)
+            hashed_email = db.Column(db.String(128))
+
+            def __init__(self, **kwargs):
+                super().__init__(**kwargs)
+                self.__set_hashed_email()
+
+            def __repr__(self):
+                return f"User('{self.name}', '{self.surname}', '{self.email}', '{self.confirmed}')"
+
+            def __set_hashed_email(self):
+                self.hashed_email = hashing.hash_value(self.email, _SALT)
+
+        self.Client = Client
+
+        try:
+            self.Client.query.filter_by(confirmed=True).all()
+        except OperationalError:
+            db.create_all()
+
+    def new_email_to_newsletter(self):
+        try:
+            email = request.form['email']
+            name = request.form['name']
+            surname = request.form['surname']
+        except exceptions.BadRequestKeyError as e:
+            return e
+
+        if not self.validate_email(email):
+            return error("InvalidEmail")
+
+        new_user = self.Client(name=name, surname=surname, email=email)
+
+        if self.client_exists(new_user):
+            return error("AlreadySignedUp")
+        elif self.email_exists_in_db(new_user):
+            return self.send_email(new_user)
+
+        try:
+            self.db.session.add(new_user)
+            self.db.session.commit()
+        except IntegrityError:
+            return error("AlreadySignedUp")
+        except OperationalError:
+            return error("Operational")
+        except Exception:
+            return error("Unexpected")
+
+        return self.send_email(new_user)
+
+    def confirm_email(self):
+        user = self.Client.query.filter_by(hashed_email=request.args.get("id")).first()
+        if user is not None:
+            if user.confirmed is True:
+                return error("AlreadySignedUp")
+            user.confirmed = True
+            self.db.session.commit()
+            return jsonify(succes=True)
+        else:
+            return error("DoesntExist")
+
+    def remove_email(self):
+        user = self.Client.query.filter_by(hashed_email=request.args.get("id"), confirmed=True).first()
+        if user is not None:
+            user.confirmed = False
+            self.db.session.commit()
+            return jsonify(succes=True)
+        else:
+            return error("DoesntExist")
+
+    def send_email(self, user):
+        try:
+            self.mail.send_message('Potwierdź rejestracje',
+                                   recipients=[str(user.email)],
+                                   html=render_template(self.template_file, id=user.hashed_email),
+                                   sender=self.mail_username)
+        except SMTPRecipientsRefused:
+            return error("InvalidEmail")
+        return jsonify(success=True)
+
+    @staticmethod
+    def create_removal_link(user):
+        return str(url_for('remove_email', id=user.hashed_email))
+
+    def client_exists(self, client):
+        if self.Client.query.filter_by(email=client.email, confirmed=True).first() is not None:
+            return True
+        else:
+            return False
+
+    def email_exists_in_db(self, email):
+        if self.Client.query.filter_by(email=email.email).first() is not None:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def validate_email(email):
+        if re.match("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
+                    email) is not None:
+            return True
+        return False
